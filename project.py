8ef13706aa9ad4bce2a1a7173d4826bbdf1f3386
IT21314742 <it21314742@my.sliit.lk>
2024-08-16 11:03:41 +0530

    Update
    Update

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ import cv2.cv2
 
 img = cv2.imread('assets/1.jpg', 1)
 img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-img = cv2.rotate(img, cv2.cv2.rotate)
+img = cv2.rotate(img, cv2.cv2.ROTATE_90)
 
 cv2.imshow('IMAGE', img)
 cv2.waitKey(0)
